cff59f094cb7667b0b0337f28d282ed79c13b0dc
danielr0007 <97550201+danielr0007@users.noreply.github.com>
2022-06-20 20:45:04 -0400

    Add files via upload

--- xxxx/src/main/resources/static/js_files/recover_password_page.js
+++ xxxx/src/main/resources/static/js_files/recover_password_page.js
@@ -1,7 +1,6 @@
-
-
-
-document.getElementById("recover_password_button").addEventListener("click",async function (e) {
+document
+  .getElementById("recover_password_button")
+  .addEventListener("click", async function (e) {
     e.preventDefault();
 
     let formData = {};
@@ -9,18 +8,20 @@ document.getElementById("recover_password_button").addEventListener("click",asyn
     formData.lasttName = document.getElementById("last_name").value;
     formData.email = document.getElementById("email").value;
 
-    console.log(formData)
+    console.log(formData);
 
-    const rawResponse = await fetch('/recoverPassword', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'content-type': 'application/json'
-
-        },
-        body: JSON.stringify(formData)
-    }).then(res=>res.json())
-        .then(data=> document.getElementById("recoveredPassword").innerText = "Your password is: "+data)
-
-
-})
+    const rawResponse = await fetch("/recoverPassword", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then(
+        (data) =>
+          (document.getElementById("recoveredPassword").innerText =
+            "Your password is: " + data)
+      );
+  });
--- xxxx/src/main/resources/static/js_files/registerUser.js
+++ xxxx/src/main/resources/static/js_files/registerUser.js
@@ -1,43 +1,52 @@
+let firstName = document.getElementById("first_name");
+let lastName = document.getElementById("last_name");
+let email = document.getElementById("email");
+let goalText = document.getElementById("goal_text");
 
+async function registerUser() {
+  event.preventDefault();
 
+  // capitalizes the user input info
+  function capitalizeUserInput(element) {
+    let FirstLetter = element.value.slice(0, 1).toUpperCase();
+    const NameCap = FirstLetter + element.value.slice(1);
+    return NameCap;
+  }
 
+  // user input in capitalized form
+  const firstNameCapitalized = capitalizeUserInput(firstName);
+  const lastNameCapitalized = capitalizeUserInput(lastName);
+  const emailCapitalized = capitalizeUserInput(email);
+  const goalTextCapitalized = capitalizeUserInput(goalText);
 
-    async function registerUser() {
-        event.preventDefault();
+  let userData = {};
+  userData.firstName = firstNameCapitalized;
+  userData.lastName = lastNameCapitalized;
+  userData.email = emailCapitalized;
+  userData.password = document.getElementById("password").value;
+  userData.goal = goalTextCapitalized;
 
-        let userData = {};
-        userData.firstName = document.getElementById("first_name").value;
-        userData.lastName = document.getElementById("last_name").value;
-        userData.email = document.getElementById("email").value;
-        userData.password = document.getElementById("password").value;
-        userData.goal = document.getElementById("goal_text").value;
+  //console.log(userData)   testing purposes
 
-        //console.log(userData)   testing purposes
+  let password2 = document.getElementById("confirm_password").value;
 
-        let password2 = document.getElementById("confirm_password").value;
+  if (password2 !== userData.password) {
+    alert("passwords don't match, enter again.");
+    document.getElementById("password").value = "";
+    document.getElementById("confirm_password").value = "";
+    return;
+  }
 
-        if (password2 !== userData.password) {
-            alert("passwords don't match, enter again.");
-            document.getElementById("password").value = "";
-            document.getElementById("confirm_password").value = "";
-            return;
-        }
-
-        const rawResponse = await fetch('/registerUser', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userData)
-        });
-        //const content = await rawResponse.json();
-        // console.log(content);
-        alert("user registered!")
-        //window.location = 'index.html';
-
-
-
+  const rawResponse = await fetch("/registerUser", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+  //const content = await rawResponse.json();
+  // console.log(content);
+  alert("user registered!");
+  //window.location = 'index.html';
 }
-
-
